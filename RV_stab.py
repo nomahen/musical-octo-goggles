338f8bffdb98d9f5feaf86cb547dd02c51381ed2
mckyrosenthal <mmrosent@ucsc.edu>
2017-04-18 15:39:50 -0700

    I changed more

--- RV_stab.py
+++ RV_stab.py
@@ -283,7 +283,7 @@ class RVSystem(RVPlanet):
 
         a0 = [planet.a for planet in ps]
 
-        stable = 2
+        stable = 1
 
         for i,t in enumerate(times): #Perform integration
             sim.integrate(t,exact_finish_time = exact)
