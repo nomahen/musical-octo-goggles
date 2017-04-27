82f0d013bd731155edf90b7c44c5484126e9cf54
cutlerycannon <cutlerycannon@gmail.com>
2017-04-27 15:22:33 -0700

    sorting fixed
    Moved sorting to its own function and out of logprob and chi_2

--- RV_stab.py
+++ RV_stab.py
@@ -4,6 +4,7 @@ import matplotlib as mpl
 import rebound
 from matplotlib.ticker import FormatStrFormatter
 import time
+from operator import attrgetter
 
 plt.rcParams['legend.frameon'] = True
 plt.rcParams['legend.fontsize'] = 18
@@ -52,9 +53,27 @@ class RVSystem(RVPlanet):
         
         self.mstar = mstar #Mass of central star
         self.planets = [] #Array containing RVPlanets class
-        self.RV_data = [] #Array of RV velocities, assumed to be of the form: JD, RV, error
+        self.RV_files = [] #Array of RV velocities, assumed to be of the form: JD, RV, error
         self.offsets = [] #Array of constant velocity offsets for each data set
         self.path_to_data = "" #Optional prefix that points to location of datasets
+        self.RV_data=[]
+    
+    def sort_data(self):    
+        JDs = []
+        vels = []
+        errs = []
+
+        for i,fname in enumerate(self.RV_files):
+            tmp_arr = np.loadtxt(self.path_to_data + fname)
+            JDs = np.concatenate((JDs,tmp_arr[:,0]))
+            vels = np.concatenate((vels,(tmp_arr[:,1]-self.offsets[i])))
+            errs = np.concatenate((errs,tmp_arr[:,2]))
+
+        #There might be a better way to do this -- these commands sort the data by time so that we can integrate
+        #up to each time
+        sort_arr = [JDs,vels,errs]
+        sort_arr = np.transpose(sort_arr)
+        self.RV_data = sort_arr[np.argsort(sort_arr[:,0])]
     
     def add_planet(self,per=365.25, mass=1, M=0, e=0, pomega=0, i=90.,Omega=0):
         """Add planet to RV simulation. Angles are in degrees, planet mass is in Jupiter masses"""
@@ -72,7 +91,7 @@ class RVSystem(RVPlanet):
         for i,planet in enumerate(self.planets):
             r = (G * (self.mstar)*msun_g * (planet.per*JD_sec)**2/(4.*np.pi**2))**(1./3.)
             r_AU = r/AU
-            print "a_%i = %.3f AU" %(i,r_AU)
+            print( "a_%i = %.3f AU" %(i,r_AU))
 
     def plot_RV(self,epoch=2450000):
 
@@ -82,7 +101,7 @@ class RVSystem(RVPlanet):
         vels = []
         errs = []
 
-        for i,fname in enumerate(self.RV_data): #Read in RV data
+        for i,fname in enumerate(self.RV_files): #Read in RV data
             tmp_arr = np.loadtxt(self.path_to_data + fname)
             JDs.append(tmp_arr[:,0])
             vels.append(tmp_arr[:,1]-self.offsets[i])
@@ -102,8 +121,8 @@ class RVSystem(RVPlanet):
                     inc=planet.i*deg2rad,Omega=planet.Omega*deg2rad)
             min_per = min(min_per,planet.per) #Minimum period, used for plotting purposes
         
-        JD_max = max(np.amax(JDs[i]) for i in range(len(self.RV_data)))
-        JD_min = min(np.amin(JDs[i]) for i in range(len(self.RV_data)))
+        JD_max = max(np.amax(JDs[i]) for i in range(len(self.RV_files)))
+        JD_min = min(np.amin(JDs[i]) for i in range(len(self.RV_files)))
         
         
         Noutputs = int((JD_max-JD_min)/min_per*100.)
@@ -124,7 +143,7 @@ class RVSystem(RVPlanet):
 
         plt.plot(times,rad_vels)
 
-        for i in range(len(self.RV_data)):
+        for i in range(len(self.RV_files)):
             plt.errorbar(JDs[i],vels[i],yerr = errs[i],fmt='o')
 
         plt.xlabel("Time [JD]")
@@ -138,21 +157,7 @@ class RVSystem(RVPlanet):
 
         """Calculate the chi^2 value of the RV time series for the planets currently in the system"""
 
-        JDs = []
-        vels = []
-        errs = []
-
-        for i,fname in enumerate(self.RV_data):
-            tmp_arr = np.loadtxt(self.path_to_data + fname)
-            JDs = np.concatenate((JDs,tmp_arr[:,0]))
-            vels = np.concatenate((vels,(tmp_arr[:,1]-self.offsets[i])))
-            errs = np.concatenate((errs,tmp_arr[:,2]))
-
-        #There might be a better way to do this -- these commands sort the data by time so that we can integrate
-        #up to each time
-        sort_arr = [JDs,vels,errs]
-        sort_arr = np.transpose(sort_arr)
-        sort_arr = sort_arr[np.argsort(sort_arr[:,0])]
+       
 
         deg2rad = np.pi/180.
         sim = rebound.Simulation()
@@ -167,7 +172,7 @@ class RVSystem(RVPlanet):
         sim.move_to_com()
         ps = sim.particles
 
-        times = sort_arr[:,0] #Times to integrate to are just the times for each data point, no need to integrate
+        times = self.RV_data[:,0] #Times to integrate to are just the times for each data point, no need to integrate
         #between data points
 
         AU_day_to_m_s = 1.731456e6
@@ -180,7 +185,7 @@ class RVSystem(RVPlanet):
         chi_2 = 0
 
         for i,vel_theory in enumerate(rad_vels):
-                chi_2 += (sort_arr[i,1]-vel_theory)**2/sort_arr[i,2]**2
+                chi_2 += (self.RV_data[i,1]-vel_theory)**2/self.RV_data[i,2]**2
 
         return chi_2
 
@@ -189,21 +194,7 @@ class RVSystem(RVPlanet):
 
         """Calculate the log likelihood for MCMC"""
 
-        JDs = []
-        vels = []
-        errs = []
-
-        for i,fname in enumerate(self.RV_data):
-            tmp_arr = np.loadtxt(self.path_to_data + fname)
-            JDs = np.concatenate((JDs,tmp_arr[:,0]))
-            vels = np.concatenate((vels,(tmp_arr[:,1]-self.offsets[i])))
-            errs = np.concatenate((errs,tmp_arr[:,2]))
-
-        #There might be a better way to do this -- these commands sort the data by time so that we can integrate
-        #up to each time
-        sort_arr = [JDs,vels,errs]
-        sort_arr = np.transpose(sort_arr)
-        sort_arr = sort_arr[np.argsort(sort_arr[:,0])]
+        
 
         deg2rad = np.pi/180.
         sim = rebound.Simulation()
@@ -218,7 +209,7 @@ class RVSystem(RVPlanet):
         sim.move_to_com()
         ps = sim.particles
 
-        times = sort_arr[:,0] #Times to integrate to are just the times for each data point, no need to integrate
+        times = self.RV_data[:,0] #Times to integrate to are just the times for each data point, no need to integrate
         #between data points
 
         AU_day_to_m_s = 1.731456e6
@@ -228,7 +219,7 @@ class RVSystem(RVPlanet):
             sim.integrate(t)
             rad_vels[i] = -ps['star'].vz * AU_day_to_m_s
 
-        return -0.5*np.sum((sort_arr[:,1]-rad_vels)**2/sort_arr[:,2]**2 + np.log(2*np.pi*sort_arr[:,2]**2))
+        return -0.5*np.sum((self.RV_data[:,1]-rad_vels)**2/self.RV_data[:,2]**2 + np.log(2*np.pi*self.RV_data[:,2]**2))
 
     def rem_planet(self,i=0):
         del self.planets[i]
@@ -250,15 +241,16 @@ class RVSystem(RVPlanet):
         # sim.t = epoch #Epoch is the starting time of simulation
         sim.add(m=self.mstar,hash='star')
 
-        min_per = np.inf
-        max_per = 0
+      
+        min_per = min(self.planets,key=attrgetter('per')).per
+        max_per = max(self.planets,key=attrgetter('per')).per
 
         for planet in self.planets: #Add planets in self.planets to Rebound simulation
             sim.add(m=planet.mass,P=planet.per,M=planet.M*deg2rad,e=planet.e,pomega=planet.pomega*deg2rad,
                     inc=planet.i*deg2rad,Omega=planet.Omega*deg2rad)
-            min_per = min(min_per,planet.per) #Minimum period
-            max_per = max(max_per,planet.per)
-
+          
+      
+        
         t_max = max_per*periods
         Noutputs = int(t_max/min_per*outputs_per_period)
         times = np.linspace(0,t_max, Noutputs)
@@ -293,18 +285,18 @@ class RVSystem(RVPlanet):
                 if plot:
                     semi_major_arr[k,i] = planet.a
             if verbose and (i % (Noutputs/10) == 0):
-                 # print "%3i %%" %(float(i+1)/float(Noutputs)*100.)
-                print "%2i %%" %(100*i/Noutputs)
+                print ("%3i %%" %(float(i+1)/float(Noutputs)*100.))
+                #print ("%2i %%" %(100*i/Noutputs))
 
             if stable == 0:
                 break
 
         if timing:
-            print "Integration took %.5f seconds" %(time.time() - start_time)
+            print ("Integration took %.5f seconds" %(time.time() - start_time))
 
         if energy_err:
             Ef = sim.calculate_energy()
-            print "Energy Error is %.3f%% " %(np.abs((Ef-E0)/E0*100))
+            print( "Energy Error is %.3f%% " %(np.abs((Ef-E0)/E0*100)))
 
 
         if plot:
@@ -315,6 +307,7 @@ class RVSystem(RVPlanet):
 
             plt.xlabel("Time [Years]")
             plt.ylabel("a [AU]")
+           
 
         return stable
 
